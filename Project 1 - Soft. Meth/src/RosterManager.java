dc9714b9ef390b445acece9e552cefd03b967853
nafriyie <nafriyie48@gmail.com>
2023-02-01 12:44:35 -0500

    Added input functionally to RosterManager class

--- Project 1 - Soft. Meth/src/RosterManager.java
+++ Project 1 - Soft. Meth/src/RosterManager.java
@@ -1,6 +1,16 @@
+// need to add packages to classes
+
+import java.util.Scanner;
+
 public class RosterManager {
 
-    public void run() {
-
+    public void run() { // will need to Scanner class to read input, either StringTokenizer or String.split() to break up command line args
+        System.out.println("Roster Manager running...");
+        Scanner scanner = new Scanner(System.in);
+        String command = scanner.nextLine();
+        String [] line = command.split("\\s+");
+        for ( String dataToken : line ) {
+            System.out.println(dataToken);
+        }
     }
 }

f911df3c20d7b01230d7ee16e943e6c60f6603fd
nafriyie <nafriyie48@gmail.com>
2023-01-31 20:47:53 -0500

    added override method signatures for Profile Class

--- Project 1 - Soft. Meth/src/Profile.java
+++ Project 1 - Soft. Meth/src/Profile.java
@@ -6,6 +6,20 @@ public class Profile implements Comparable<Profile> { // implements Comparable a
     public Profile(String lname, String fname, Date dob) {
         this.lname = lname;
         this.fname = fname;
-        this.dob = dob;
+        this.dob = new Date(dob);
+    }
+
+    @Override
+    public String toString(){
+        return null;
+    }
+
+    @Override
+    public int compareTo(Object obj){
+        return 0;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        return false;
     }
 }

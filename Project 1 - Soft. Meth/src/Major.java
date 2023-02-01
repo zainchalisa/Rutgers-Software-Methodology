846e91e36c2b166337639840eadda234dca665cb
nafriyie <nafriyie48@gmail.com>
2023-01-31 22:12:26 -0500

    added functionality to Major class

--- Project 1 - Soft. Meth/src/Major.java
+++ Project 1 - Soft. Meth/src/Major.java
@@ -1,2 +1,17 @@
 public enum Major {
+
+    CS ("01:198","SAS"),
+    MATH ("01:640","SAS"),
+    EE ("14:332","SOE"),
+    ITI ("04:547","SC&I"),
+    BAIT ("33:136","RBS")
+
+    private final String coreCode;
+    private final String school;
+
+    Major(String coreCode, String school) {
+        this.coreCode = coreCode;
+        this.school = school;
+    }
+
 }

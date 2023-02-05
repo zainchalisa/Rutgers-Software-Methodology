7dfc734ee0577d7d2817b8f5c6e3ef7c74d1532a
nafriyie <nafriyie48@gmail.com>
2023-02-04 21:41:57 -0500

    updated RosterManager Class

--- Project 1 - Soft. Meth/src/RosterManager.java
+++ Project 1 - Soft. Meth/src/RosterManager.java
@@ -4,17 +4,48 @@ import java.util.Scanner;
 
 public class RosterManager {
 
+
+    private void checkMajor(String major, Major majorName) { // finish helper method
+        if ( major.equals("CS") ) {
+            majorName = Major.CS;
+        }
+        else if (major.equals("MATH")) {
+            majorName = Major.MATH;
+        }
+        else if (major.equals("EE")) {
+            majorName = Major.EE;
+        }
+        else if (major.equals("ITI")) {
+            majorName = Major.ITI;
+        }
+        else if (major.equals("BAIT")) {
+            majorName = Major.BAIT;
+        }
+    }
+
+
     public void run() { // will need to Scanner class to read input, either StringTokenizer or String.split() to break up command line args
         System.out.println("Roster Manager running...");
         Scanner scanner = new Scanner(System.in);
         String dataToken = scanner.nextLine();
         String [] line = dataToken.split("\\s+");
 
-        //String command
+        String command = line[0];
+        String firstName = line[1];
+        String lastName = line[2];
+        String dateOfBirth = line[3];
+        String major = line[4];
 
-        for ( String word : line ) {
+        Major majorName = null;
+
+        checkMajor(major,majorName);
+
+        String creditCompleted = line[5];
+        int tokenCount = 0;
+        while ( tokenCount < line.length ) {
             if ( line[0].equals("A") ) {
-                //Student student = new Student(new Profile(line[2],line[1],))
+                Student student = new Student(new Profile(lastName,firstName,new Date(dateOfBirth)),null,creditCompleted);
+                //Roster.add(student);
             }
             else if ( line[0].equals("R") ) {
                 System.out.println("Run command R");
@@ -41,6 +72,7 @@ public class RosterManager {
             else {
                 System.out.println("Error, command not valid.");
             }
+            tokenCount++;
         }
 
     }

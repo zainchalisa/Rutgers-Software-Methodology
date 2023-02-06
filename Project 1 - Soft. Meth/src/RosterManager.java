2ea9f73ce29fc5aa0308dce42e22a3f2d237467d
nafriyie <nafriyie48@gmail.com>
2023-02-05 21:07:48 -0500

    updated loop and add command in Roster Manager

--- Project 1 - Soft. Meth/src/RosterManager.java
+++ Project 1 - Soft. Meth/src/RosterManager.java
@@ -4,63 +4,48 @@ import java.util.Scanner;
 
 public class RosterManager {
 
-    /*
-    private void checkMajor(String major, Major majorName) { // finish helper method
-        if ( major.equals("CS") ) {
-            majorName = Major.CS;
-        }
-        else if (major.equals("MATH")) {
-            majorName = Major.MATH;
-        }
-        else if (major.equals("EE")) {
-            majorName = Major.EE;
-        }
-        else if (major.equals("ITI")) {
-            majorName = Major.ITI;
-        }
-        else if (major.equals("BAIT")) {
-            majorName = Major.BAIT;
-        }
-    } */
-
+    private static final int ROSTER_SIZE = 4;
 
     public void run() { // will need to Scanner class to read input, either StringTokenizer or String.split() to break up command line args
         System.out.println("Roster Manager running...");
         Scanner scanner = new Scanner(System.in);
-        String dataToken = scanner.nextLine();
-        String [] line = dataToken.split("\\s+");
+        String dataToken = "";
 
-        String command = line[0];
-        String firstName = line[1];
-        String lastName = line[2];
-        String dateOfBirth = line[3];
-        String major = line[4];
-        String creditCompletedString = line[5];
-        int creditCompleted = Integer.parseInt(creditCompletedString);
+        while ( !dataToken.equals("Q") ) {
+            dataToken = scanner.nextLine();
+            String [] line = dataToken.split("\\s+");
+            String command = line[0];
 
-        Major majorName = null;
-
-        if ( major.equals("CS") ) {
-            majorName = Major.CS;
-        }
-        else if (major.equals("MATH")) {
-            majorName = Major.MATH;
-        }
-        else if (major.equals("EE")) {
-            majorName = Major.EE;
-        }
-        else if (major.equals("ITI")) {
-            majorName = Major.ITI;
-        }
-        else if (major.equals("BAIT")) {
-            majorName = Major.BAIT;
-        }
-
-        int tokenCount = 0;
-        while ( tokenCount < line.length ) {
             if ( command.equals("A") ) {
+                String firstName = line[1];
+                String lastName = line[2];
+                String dateOfBirth = line[3];
+                String major = line[4];
+                String creditCompletedString = line[5];
+                int creditCompleted = Integer.parseInt(creditCompletedString);
+
+                Major majorName = null;
+
+                if ( major.equals("CS") ) {
+                    majorName = Major.CS;
+                }
+                else if (major.equals("MATH")) {
+                    majorName = Major.MATH;
+                }
+                else if (major.equals("EE")) {
+                    majorName = Major.EE;
+                }
+                else if (major.equals("ITI")) {
+                    majorName = Major.ITI;
+                }
+                else if (major.equals("BAIT")) {
+                    majorName = Major.BAIT;
+                }
+
                 Student student = new Student(new Profile(lastName,firstName,new Date(dateOfBirth)),majorName,creditCompleted);
-                //Roster.add(student);
+                Roster roster = new Roster(new Student[ROSTER_SIZE],ROSTER_SIZE);
+                roster.add(student);
+                System.out.println(firstName + " " + lastName + " " + dateOfBirth + " added to the roster.");
             }
             else if ( command.equals("R") ) { // Command R takes in less commands then command A, new a seperate method for each command
                 System.out.println("Run command R");
@@ -81,14 +66,14 @@ public class RosterManager {
                 System.out.println("Run command PC");
             }
             else if ( command.equals("Q") ) {
-                System.out.println("Roster Manager terminated.");
                 break;
             }
             else {
-                System.out.println("Error, command not valid.");
+                System.out.println(command + " is not a valid command!");
             }
-            tokenCount++;
+
         }
+        System.out.println("Roster Manager Terminated");
 
     }
 }

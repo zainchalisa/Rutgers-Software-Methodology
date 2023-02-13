c8ce33bde91270736a2ac4e2aaf2c488ccf3acc7
zainchalisa <zain.chalisa@gmail.com>
2023-02-13 17:15:31 -0500

    complete

--- Project 1 - Soft. Meth/src/project1/RunProject1.java
+++ Project 1 - Soft. Meth/src/project1/RunProject1.java
@@ -3,6 +3,8 @@ package project1;
 /**
  * This method runs the project and builds it for user input
  * in the terminal
+ * @author zainchalisa
+ * @author nanaafriyie
  */
 public class RunProject1 {
     public static void main(String[] args) {

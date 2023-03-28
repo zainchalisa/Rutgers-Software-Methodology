c3aae2313d41ff9403ab7dd2bd436109b4620eb4
nafriyie <nafriyie48@gmail.com>
2023-03-27 23:47:09 -0400

    added variables to coffee controller

--- Project 4 - Soft. Meth/src/main/java/project4/project4/OrderingCoffeeController.java
+++ Project 4 - Soft. Meth/src/main/java/project4/project4/OrderingCoffeeController.java
@@ -1,22 +1,41 @@
 package project4.project4;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class OrderingCoffeeController {
 
     private CafeStoreMainController mainController;
 
     @FXML
-    private Label welcomeText;
+    private ComboBox<String> coffeeCupSizes;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private ListView<String> coffeeToppings;
+
+    @FXML
+    private Button addCoffee;
+
+    @FXML
+    private Button removeCoffee;
+
+    @FXML
+    private ListView<String> coffeeShoppingCart;
+
+    @FXML
+    private TextField coffeeSubtotal;
+
+    private ObservableList<String> coffeeCupSizesList;
 
     @FXML
     public void setMainController(CafeStoreMainController cafeStoreMainController) {
             mainController = cafeStoreMainController;
+    }
+
+    public void initialize () {
+        coffeeCupSizesList = FXCollections.observableArrayList("Short","Tall","Grande","Venti");
+        coffeeCupSizes.setItems(coffeeCupSizesList);
     }
 }

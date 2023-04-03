60a615dc472bfe18511483f3d302e126d40c8fd3
nafriyie <nafriyie48@gmail.com>
2023-04-03 18:33:17 -0400

    added functionality to cancel order button in store orders tab

--- Project 4 - Soft. Meth/src/main/java/project4/project4/StoreOrdersController.java
+++ Project 4 - Soft. Meth/src/main/java/project4/project4/StoreOrdersController.java
@@ -51,20 +51,36 @@ public class StoreOrdersController {
         }
     }
 
+    // orders is the name of the combo box that holds the order numbers
+    // holderOrder is the specific order the user has selceted (return list of menu items in that order)
+    // orderList is the list of orders that have been placed by the user from the basket tab
+
     @FXML
     public void cancelOrder(ActionEvent cancelOrder){
         holderOrder = FXCollections.observableArrayList(orders.getValue().getOrderList());
-        System.out.println("Current Order" + holderOrder);
-        System.out.println("Order List" + orders.getValue());
-        System.out.println("All Orders" + orderList);
+        //System.out.println("Current Order" + holderOrder);
+//        System.out.println("Order List" + orders.getValue());
+//        System.out.println("All Orders" + orderList); // holds order number, not actual values
         for(Order order: orderList){
-            if(order == holderOrder){
-                orderList.remove(orders.getValue().getOrderList());
+            System.out.println(order);
+            System.out.println(orders.getValue());
+            //System.out.println(order.equals(orders.getValue()));
+            if(order.equals(orders.getValue())){ // works
+                if (orderList.size() == 1 || order.equals(orderList.get(0))) {
+                    System.out.println("1st conditional");
+                    contentOfOrder.getItems().clear();
+                    orders.getItems().remove(orders.getValue());
+                    orders.setValue(null);
+                    totalAmount.setText(decimalFormat.format(Coffee.STARTING_TOTAL));
+                    break;
+                }
+                orders.getItems().remove(orders.getValue());
+                break;
             }
         }
         System.out.println("All Orders After Remove" + orderList);
         orders.setItems(orderList);
-        System.out.println(orders.getValue().getOrderList());
+        //System.out.println(orders.getValue().getOrderList());
     }
 
     @FXML
@@ -79,11 +95,13 @@ public class StoreOrdersController {
 
     @FXML
     public void getSelectedOrder(ActionEvent selectOrder){
-        holderOrder = FXCollections.observableArrayList(orders.getValue().getOrderList());
-        contentOfOrder.setItems(holderOrder);
-        System.out.println(holderOrder);
-        System.out.println(getTotalAmount());
-        totalAmount.setText(decimalFormat.format(getTotalAmount()));
+        if (orders.getValue() != null) {
+            holderOrder = FXCollections.observableArrayList(orders.getValue().getOrderList());
+            contentOfOrder.setItems(holderOrder);
+            System.out.println(holderOrder);
+            System.out.println(getTotalAmount());
+            totalAmount.setText(decimalFormat.format(getTotalAmount()));
+        }
     }
 
     private double getSubtotal() {

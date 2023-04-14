e002dcee4eaff24848a62058b6e1602c5b6cfb29
zainchalisa <zain.chalisa@gmail.com>
2023-04-13 22:49:50 -0400

    completed UI except donut

--- Project 5 - Soft. Meth/app/src/main/java/project5/project5/CoffeeOrder.java
+++ Project 5 - Soft. Meth/app/src/main/java/project5/project5/CoffeeOrder.java
@@ -1,4 +1,26 @@
 package project5.project5;
 
-public class CoffeeOrder {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CoffeeOrder extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coffee_order);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
 }
--- Project 5 - Soft. Meth/app/src/main/java/project5/project5/MainActivity.java
+++ Project 5 - Soft. Meth/app/src/main/java/project5/project5/MainActivity.java
@@ -1,7 +1,10 @@
 package project5.project5;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       ImageButton coffeeButton = findViewById(R.id.CoffeeOrderButton);
+       coffeeButton.setOnClickListener(v ->
+               startActivity(new Intent(MainActivity.this, CoffeeOrder.class)));
+
+
     }
 }

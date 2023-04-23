fdd524a456e3a8bbfcab1d10130ee6c3bd58070f
zainchalisa <zain.chalisa@gmail.com>
2023-04-23 19:05:59 -0400

    worked on contraints

--- Project 5 - Soft. Meth/app/src/main/java/project5/project5/CoffeeOrder.java
+++ Project 5 - Soft. Meth/app/src/main/java/project5/project5/CoffeeOrder.java
@@ -3,16 +3,71 @@ package project5.project5;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class CoffeeOrder extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private CheckBox sweetCream;
+    private CheckBox frenchVanilla;
+    private CheckBox irishCream;
+    private CheckBox caramel;
+    private CheckBox mocha;
+    public static final String SWEET_CREAM = "Sweet Cream";
+    public static final String FRENCH_VANILLA = "French Vanilla";
+    public static final String IRISH_CREAM = "Irish Cream";
+    public static final String CARAMEL = "Caramel";
+    public static final String MOCHA = "Mocha";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coffee_order);
 
+    }
+
+    public void addItem(){
+
+        ArrayList<String> toppings = new ArrayList<>();
+        toppingsSelected(toppings);
+
+
+
+
+    }
+
+    public void toppingsSelected(ArrayList<String> toppings){
+
+        toppings.remove(SWEET_CREAM);
+        toppings.remove(FRENCH_VANILLA);
+        toppings.remove(IRISH_CREAM);
+        toppings.remove(CARAMEL);
+        toppings.remove(MOCHA);
+
+
+        if(sweetCream.isChecked()){
+            toppings.add(SWEET_CREAM);
+        }
+
+        if(frenchVanilla.isChecked()){
+            toppings.add(FRENCH_VANILLA);
+        }
+
+        if(irishCream.isChecked()){
+            toppings.add(IRISH_CREAM);
+        }
+
+        if(caramel.isChecked()){
+            toppings.add(CARAMEL);
+        }
+
+        if(mocha.isChecked()){
+            toppings.add(MOCHA);
+        }
     }
 
     @Override

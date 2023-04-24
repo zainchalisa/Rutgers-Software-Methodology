3f77a9037f8e2ab7b51edc9c24e093db57d72a9c
zainchalisa <zain.chalisa@gmail.com>
2023-04-24 15:50:32 -0400

    completed coffeeOrder

--- Project 5 - Soft. Meth/app/src/main/java/project5/project5/CoffeeOrder.java
+++ Project 5 - Soft. Meth/app/src/main/java/project5/project5/CoffeeOrder.java
@@ -33,6 +33,13 @@ public class CoffeeOrder extends AppCompatActivity implements AdapterView.OnItem
     public static final String CARAMEL = "Caramel";
     public static final String MOCHA = "Mocha";
 
+    public static final int SHORT_INDEX = 0;
+    public static final int TALL_INDEX = 1;
+    public static final int GRANDE_INDEX = 2;
+    public static final int VENTI_INDEX = 3;
+    public static final int INCREMENT = 1;
+    public static final int ZERO = 0;
+
     private ArrayList<String> coffeeSizesList = new ArrayList<>(List.of("Short", "Tall", "Grande", "Venti"));
     private ArrayList<Integer> coffeeQuantityList = new ArrayList<>(List.of(1,2,3,4,5));
     public static Order coffeeOrder = new Order();
@@ -40,24 +47,83 @@ public class CoffeeOrder extends AppCompatActivity implements AdapterView.OnItem
     public CoffeeOrder() {
     }
 
+    public void createItems(){
+        sweetCream = findViewById(R.id.sweetCream);
+        irishCream = findViewById(R.id.irishCream);
+        caramel = findViewById(R.id.caramel);
+        mocha = findViewById(R.id.mocha);
+        frenchVanilla = findViewById(R.id.frenchVanilla);
+        coffeeSizes = findViewById(R.id.coffeeSizes);
+        addToOrderButton = findViewById(R.id.addToOrderButton);
+        quantity = findViewById(R.id.quantity);
+        subtotalAmount = findViewById(R.id.subtotalAmount);
+    }
+
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coffee_order);
-        coffeeSizes = findViewById(R.id.coffeeSizes);
+        createItems();
         ArrayAdapter<String> coffeeSizeAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, coffeeSizesList);
         coffeeSizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         coffeeSizes.setAdapter(coffeeSizeAdapter);
-        quantity = findViewById(R.id.quantity);
         ArrayAdapter<Integer> quantityAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, coffeeQuantityList);
         coffeeSizeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         quantity.setAdapter(quantityAdapter);
-        /*addToOrderButton.setOnClickListener(view -> {
+        addToOrderButton.setOnClickListener(view -> {
             addItem();
-            //updateBalance();
-        });*/
+        });
+        checkBoxListener();
+        quantityListener();
+        coffeeSizeListener();
+    }
 
+    public void checkBoxListener(){
+        irishCream.setOnClickListener(view ->{
+            subtotalAmount();
+        });
+        sweetCream.setOnClickListener(view ->{
+            subtotalAmount();
+        });
+        frenchVanilla.setOnClickListener(view ->{
+            subtotalAmount();
+        });
+        caramel.setOnClickListener(view ->{
+            subtotalAmount();
+        });
+        mocha.setOnClickListener(view ->{
+            subtotalAmount();
+        });
+    }
+
+    public void quantityListener(){
+        quantity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                subtotalAmount();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    public void coffeeSizeListener(){
+        coffeeSizes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                subtotalAmount();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
     public void addItem(){
@@ -80,13 +146,16 @@ public class CoffeeOrder extends AppCompatActivity implements AdapterView.OnItem
         irishCream.setChecked(false);
         caramel.setChecked(false);
         mocha.setChecked(false);
+        quantity.setSelection(ZERO);
+        coffeeSizes.setSelection(ZERO);
+        subtotalAmount.setText(String.format("$1.89"));
     }
 
     public Coffee newCoffeeItem(){
         int coffeeSizeIndex = coffeeSizes.getSelectedItemPosition();
         String coffeeSize = getCoffeeSize(coffeeSizeIndex);
         int coffeeQuantityIndex = quantity.getSelectedItemPosition();
-        int coffeeQuantity = coffeeQuantityIndex + 1;
+        int coffeeQuantity = coffeeQuantityIndex + INCREMENT;
         ArrayList<String> toppings = new ArrayList<>();
         toppingsSelected(toppings);
         Coffee newCoffee = new Coffee(coffeeSize, toppings);
@@ -95,13 +164,13 @@ public class CoffeeOrder extends AppCompatActivity implements AdapterView.OnItem
     }
 
     public String getCoffeeSize(int coffeeSizeIndex){
-        if(coffeeSizeIndex == 0){
+        if(coffeeSizeIndex == SHORT_INDEX){
             return "Short";
-        } else if(coffeeSizeIndex == 1){
+        } else if(coffeeSizeIndex == TALL_INDEX){
             return "Tall";
-        } else if(coffeeSizeIndex == 2){
+        } else if(coffeeSizeIndex == GRANDE_INDEX){
             return "Grande";
-        } else if(coffeeSizeIndex == 3){
+        } else if(coffeeSizeIndex == VENTI_INDEX){
             return "Venti";
         } else{
             return "";
@@ -110,11 +179,10 @@ public class CoffeeOrder extends AppCompatActivity implements AdapterView.OnItem
 
     public void subtotalAmount(){
         Coffee currentCoffee = newCoffeeItem();
-        subtotalAmount.setText(String.valueOf(currentCoffee.itemPrice()));
+        subtotalAmount.setText(String.format("$" + "%.2f", currentCoffee.itemPrice()));
     }
 
     public void toppingsSelected(ArrayList<String> toppings){
-
         toppings.remove(SWEET_CREAM);
         toppings.remove(FRENCH_VANILLA);
         toppings.remove(IRISH_CREAM);

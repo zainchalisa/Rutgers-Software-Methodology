5235bdec4af6162e1a342d74d21561ca740c3ecc
zainchalisa <zain.chalisa@gmail.com>
2023-04-24 19:07:01 -0400

    fixed UI for currentOrders

--- Project 5 - Soft. Meth/app/src/main/java/project5/project5/CurrentOrder.java
+++ Project 5 - Soft. Meth/app/src/main/java/project5/project5/CurrentOrder.java
@@ -3,6 +3,8 @@ package project5.project5;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.ObservableArrayList;
@@ -16,6 +18,11 @@ public class CurrentOrder extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_order);
+        String[] items = {"Item 1", "Item 2", "Item 3"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, items);
+        ListView listView = findViewById(R.id.order_list);
+        listView.setAdapter(adapter);
 
     }
 

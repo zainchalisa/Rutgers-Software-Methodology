84903fce0a682aa2c3ca178b4eeb82bbc0a25464
nafriyie <nafriyie48@gmail.com>
2023-04-25 12:20:25 -0400

    calculated prices of list

--- Project 5 - Soft. Meth/app/src/main/java/project5/project5/CurrentOrder.java
+++ Project 5 - Soft. Meth/app/src/main/java/project5/project5/CurrentOrder.java
@@ -1,9 +1,11 @@
 package project5.project5;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -16,11 +18,20 @@ import java.util.ArrayList;
 
 public class CurrentOrder extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    private ListView order_list;
     private TextView orderNumberView;
     private TextView subtotalView;
     private TextView salesTaxView;
     private TextView totalAmountView;
+    private Button placeOrderButton;
+
+    private Order order = new Order();
     private static ObservableArrayList<MenuItem> currentOrders = new ObservableArrayList<>();
+
+    private double runningSubtotal;
+    private double runningSalesTax;
+    private double runningTotal;
+    public static final double SALES_TAX = .06625;
     public static ObservableArrayList<MenuItem> getOrder() {
         return currentOrders;
     }
@@ -31,7 +42,20 @@ public class CurrentOrder extends AppCompatActivity implements AdapterView.OnIte
         ArrayAdapter<MenuItem> adapter = new ArrayAdapter<MenuItem>(this, android.R.layout.simple_list_item_1, currentOrders);;
         ListView listView = findViewById(R.id.order_list);
         listView.setAdapter(adapter);
+        order.setOrderList(currentOrders);
+        order_list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         createViews();
+        calculateCart();
 
     }
 
@@ -40,6 +64,25 @@ public class CurrentOrder extends AppCompatActivity implements AdapterView.OnIte
         subtotalView = findViewById(R.id.subtotalView);
         salesTaxView = findViewById(R.id.salesTaxView);
         totalAmountView = findViewById(R.id.totalAmountView);
+        placeOrderButton = findViewById(R.id.placeOrderButton);
+
+        placeOrderButton.setText("Place Order");
+    }
+
+    private void calculateCart() {
+        for (MenuItem item : currentOrders) {
+            runningSubtotal += item.itemPrice();
+        }
+        runningSalesTax = runningSubtotal * SALES_TAX;
+        runningTotal = runningSubtotal + runningSalesTax;
+
+        subtotalView.setText("$" + String.format("%.2f",runningSubtotal));
+        salesTaxView.setText("$" + String.format("%.2f",runningSalesTax));
+        totalAmountView.setText("$" + String.format("%.2f",runningTotal));
+    }
+
+    private void deleteItem() {
+
     }
 
     public static void addToBasket(MenuItem item) {
